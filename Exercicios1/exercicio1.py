f3691fb5f5a18ced3f78eeef5b172b28615e4bc7
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2023-09-08 15:22:27 -0400

    Lista 1 Exercicio2 feito.

---
+++ Exercicios1/exercicio1.py
@@ -0,0 +1,9 @@
+# 1. Crie duas variáveis: nome e idade. Atribua seu nome a nome e sua idade a idade. Imprima uma
+# mensagem usando essas vari´aveis.
+
+print ("### Informacoes de ###")
+
+nome = input ("Informe o seu nome: ")
+idade = int(input("Informe a sua idade: "))
+
+print("Meu nome eh",nome ,"e minha idade eh ", idade,".")
---
+++ Exercicios1/exercicio2.py
@@ -0,0 +1,12 @@
+# 2. Peça ao usuário para inserir dois números. Realize as quatro operações aritméticas básicas (adição,
+# subtração, multiplicação e divisão) com esses números e imprima os resultados.
+
+print("##MiniCalculadora##")
+
+numero1 = float(input("informe o primeiro numero: "))
+numero2 = float(input("informe o segundo numero: "))
+
+print("A soma de ", numero1,"+",numero2," =",numero1+numero2,".")
+print("A subtracao de ", numero1,"-",numero2," =",numero1-numero2,".")
+print("A multiplicacao de ", numero1,"*",numero2," =",numero1*numero2,".")
+print("A divisao de ", numero1,"/",numero2," =",numero1/numero2,".")
